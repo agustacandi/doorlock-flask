ed22f53d96156f38a948c4a2c51e9a3da0b95e33
FlyingCat17 <fthnmrh012@gmail.com>
2023-11-29 11:45:30 +0700

    update last before penilaian

--- labels.py
+++ labels.py
@@ -1,13 +1,22 @@
 import io
+import json
 
 label = []
 
-with io.open('static/labels/labels.txt', 'r', encoding='utf-8') as f:
-    labels = f.readlines()
-    labels = [line.rstrip('\n') for line in labels]
-    print(labels)
-    for i in labels:
-      label.append(i)
-for i in label:
-  split = i.split('-')
-  print(split[0])
+# with io.open('static/labels/labels.txt', 'r', encoding='utf-8') as f:
+    # labels = f.readlines()
+    # labels = [line.rstrip('\n') for line in labels]
+    # print(labels)
+    # for i in labels:
+      # label.append(i)
+# for i in label:
+  # split = i.split('-')
+  # print(split[0])
+  
+
+with open('static/labels/labels.json', 'r') as json_file:
+  data = json.load(json_file)
+  
+for i in data:
+  label.append(i)
+  print(i)
